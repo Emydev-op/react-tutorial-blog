9a6c80fb4061ee6f8272594a432153f891dfaa56
Emydev-op <alphaoseghe@gmail.com>
2023-09-20 18:50:41 +0100

    understanding useEffect hook dependencies

--- src/Home.js
+++ src/Home.js
@@ -9,6 +9,8 @@ const Home = () => {
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ]);
 
+    const [ name, setName ] = useState('mario')
+
     const handleDelete = (id) => {
         const newBlog = blogs.filter(item => item.id !== id)
         setBlogs(newBlog)
@@ -16,11 +18,13 @@ const Home = () => {
 
     useEffect(() => {
         alert("useeffect ran")
-    }, []);
+    }, [name]);
 
     return ( 
         <div className="home">
             <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+            <button onClick={() => setName('Saske')}>Change</button>  
+            <p>{ name }</p>
         </div>
      );
 }
