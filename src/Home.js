9b4e7125575915a6e2d70b0468f04dccef75cd52
Emydev-op <alphaoseghe@gmail.com>
2023-09-20 02:27:01 +0100

    outing list

--- src/Home.js
+++ src/Home.js
@@ -2,9 +2,20 @@ import { useState } from "react";
 
 const Home = () => {
 
+    const [blog, setBlog] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ]);
+
     return ( 
         <div className="home">
-
+            {blog.map((list) => (
+                <div className="blog-preview" key={list.id}>
+                    <h2>{ list.title }</h2>
+                    <p>Written by {list.author}</p>
+                </div>
+            ))}
         </div>
      );
 }
