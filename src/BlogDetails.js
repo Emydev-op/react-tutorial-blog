e93acdde85e46addacece347690aaa8cf29224d5
Emydev-op <alphaoseghe@gmail.com>
2023-09-22 17:53:30 +0100

    Created individual blog

---
+++ src/BlogDetails.js
@@ -0,0 +1,23 @@
+import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import useFetch from "./useFetch";
+
+const BlogDetails = () => {
+
+const { id } = useParams();
+const { data:blog, isPending, error } = useFetch('http://localhost:8000/blogs/' + id );
+
+    return ( 
+        <div className="blog-details">
+            {isPending && <div>Loading...</div>}
+            {error && <div> {error} </div>}
+            {blog && (
+                <article>
+                    <h2>Written by {blog.author}</h2>
+                    <div>{blog.body}</div>
+                </article>
+            )}
+        </div>
+     );
+}
+ 
+export default BlogDetails;
