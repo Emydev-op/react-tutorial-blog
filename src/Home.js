6b176c7b08257eee32951104df8f9ae62655694b
Emydev-op <alphaoseghe@gmail.com>
2023-09-22 11:40:03 +0100

    fetching data inside the useEffect hook

--- src/Home.js
+++ src/Home.js
@@ -4,20 +4,33 @@ import BlogList from "./BlogList";
 const Home = () => {
 
     const [blogs, setBlogs] = useState(null);
-
-    const [ name, setName ] = useState('mario')
+    const [isPending, setPending] = useState(true);
+    const [ error, setError ] = useState(null);
 
     useEffect(() => {
         fetch('http://localhost:8000/blogs')
-        .then(res => res.json())
-        .then((data) => setBlogs(data))
+        .then(res => {
+            if (!res.ok) {
+                throw Error('Couldn\'t connect to server');
+            }
+            return res.json()
+        })
+        .then((data) => {
+            setBlogs(data); 
+            setPending(false)
+            setError(null);
+        })
+        .catch((err) => {
+            setError(err.message);
+            setPending(false);
+        })
     }, []);
 
     return ( 
         <div className="home">
+            {error && <div>{ error }</div>}
+            {isPending && <div>Loading...</div>}
             {blogs && <BlogList blogs={blogs} title="All Blogs" />}
-            <button onClick={() => setName('Saske')}>Change</button>  
-            <p>{ name }</p>
         </div>
      );
 }
